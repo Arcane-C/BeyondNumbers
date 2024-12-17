7f62a377a232bf18837568b1ec8401207f22515f
Arcane-C <fuericitasu@gmail.com>
2024-12-17 15:39:07 +0700

    Background?

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,9 +6,11 @@ public class GameController : MonoBehaviour
 {
     public StoryScene currentScene;
     public BottomBarController bottomBar; 
+    public BackgroundController backgroundController;
     void Start()
     {
         bottomBar.PlayScene(currentScene);
+        backgroundController.SetImage(currentScene.background);
     }
 
     // Update is called once per frame
@@ -24,6 +26,7 @@ public class GameController : MonoBehaviour
                     {
                         currentScene = currentScene.nextScene;
                         bottomBar.PlayScene(currentScene);
+                        backgroundController.SwitchImage(currentScene.background);
 
                     }
                 }
